8091400e6a78380f3344b9ed04a9edfbe00ac4bc
Yamada <85409185+DevYamada@users.noreply.github.com>
2025-08-14 14:39:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Phone.Models;
+
+
+Console.WriteLine("Iphone");
+Iphone iphone = new Iphone(numero: "123456", modelo: "Modelo 98", imei: "444444", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Nokia");
+Nokia nokia = new Nokia(numero: "876541", modelo: "Modelo 12", imei: "222222", memoria: 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
